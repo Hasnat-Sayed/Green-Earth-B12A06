3a969b4a7589b87f32e415246596c038df4b6eba
Hasnat-Sayed <hasnatbin1@gmail.com>
2025-09-09 13:00:04 +0600

    fetched and loaded trees

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,77 @@ const displayCategory = (categories) => {
 };
 
 loadCategory()
+
+
+//loading spinner
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("card-container").classList.add("hidden");
+  } else {
+    document.getElementById("card-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
+
+//button active state
+const removeActive = () => {
+    const categoryButtons = document.querySelectorAll(".category-btn")
+    categoryButtons.forEach((btn) => btn.classList.remove("active"))
+} 
+
+//load and display trees
+const loadTrees = (id) => {
+    manageSpinner(true);
+
+    const url =
+        id === "all"
+            ? "https://openapi.programming-hero.com/api/plants"
+            : `https://openapi.programming-hero.com/api/category/${id}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            removeActive();
+
+            const clickBtn = document.getElementById(`category-btn-${id}`);
+            if (clickBtn) {
+                clickBtn.classList.add("active");
+            }
+
+            const plants = Array.isArray(data) ? data : data.plants;
+
+            displayTrees(plants);
+        });
+};
+
+const displayTrees = (trees) => {
+    const treeContainer = document.getElementById("card-container");
+    treeContainer.innerHTML = "";
+
+    trees.forEach((tree) => {
+
+        const card = document.createElement("div");
+        card.innerHTML =
+            `<div class="single-card bg-white p-4 rounded-lg flex flex-col h-full shadow-sm">
+							<div class="card-image">
+								<img class="h-64 object-cover w-full rounded-lg" src="${tree.image}" alt="">
+							</div>
+							<div class="card-heading flex-1">
+								<h1 onclick="loadTreeDetail('${tree.id}')" class="font-semibold mt-3 cursor-pointer">${tree.name}</h1>
+							</div>
+							<div class="card-description flex-1">
+								<p class="text-xs font-normal text-[#1F2937] my-2">${tree.description}</p>
+							</div>
+							<div class="card-price-tag flex items-center justify-between mb-3">
+								<p class="text-sm  font-semibold text-[#15803D] px-3 py-1 bg-[#DCFCE7] rounded-full">${tree.category}</p>
+								<p class="text-sm font-semibold">৳${tree.price}</p>
+							</div>
+							<div class="card-button">
+								<button class="text-base font-semibold text-white bg-[#15803D] py-2 px-3 w-full rounded-full cursor-pointer">Add to Cart</button>
+							</div>
+						</div>`;
+        treeContainer.append(card);
+    });
+    manageSpinner(false)
+};
